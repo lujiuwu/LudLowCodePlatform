b1dcc4e8f03875306bb270882627a21b41ce1fe9
deer-nine-witches <731814625@qq.com>
2025-05-08 20:29:55 +0800

    update files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/LudLowCodePlatform/', // 替代 base
+  outputDir: 'docs' // 替代 build.outDir
 })
